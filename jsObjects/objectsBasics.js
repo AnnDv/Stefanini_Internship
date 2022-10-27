6c8d1211c48746e2c0664c8963e31b51284dfba9
Dvorac <advorac@techteam.com>
2022-10-27 13:19:49 +0300

    feat: object basics

---
+++ jsObjects/objectsBasics.js
@@ -0,0 +1,57 @@
+// Task 1 - hello objects
+let user = {
+    name: "John",
+    surname: "Smith",
+}
+
+user.name = "Pete"; // change the value of the name
+console.log(user.name);
+delete user.name; // delete the property
+console.log(user);
+
+// Task 2 - check the emptiness
+function isEmpty(schedule) {
+    for(i in schedule) {
+        return false
+    }
+    return true
+    
+}
+
+let schedule = {};
+console.log(isEmpty(schedule));
+schedule["8:30"] = "get up";
+console.log(isEmpty(schedule));
+
+// Task 3 - sum object properties
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+
+// function to check if the object is empty or not
+function checkSalary(salary) {
+    for(j in salaries) {
+        let sum = salaries.John + salaries.Ann + salaries.Pete;
+        return sum;
+    }
+    return 0;
+}
+
+console.log(checkSalary(salaries));
+
+// Task 4 - multiply numeric property values by 2 
+function multiplyNumeric(obj) {
+    for(i in obj) {
+        if(typeof obj[i] == 'number') {
+            return obj[i] *= 2
+        }
+    }
+}
+
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+};
