15903ff887f0dca6396fef0a7ded9c99ebfdebb4
Dvorac <advorac@techteam.com>
2022-11-17 23:13:53 +0200

    feat: Add fetch unit test

--- reactMovieApp/src/features/__test__/FetchMovies.test.js
+++ reactMovieApp/src/features/__test__/FetchMovies.test.js
@@ -14,4 +14,15 @@ describe('FetchMovies', () => {
       expect(movieCollLength).toBeGreaterThan(1);
     });
   });
+
+  it('should contain class name "card_description"', async () => {
+    const { getByTestId } = render(<FetchMovies />);
+    await waitFor(async () => {
+      const movieColl = getByTestId('fetch').children[1].children[0];
+      //  const movieCollLength = movieColl.childElementCount;
+      // console.log(movieCollLength);
+      const movieCollHaveClass = movieColl.getElementsByClassName('card_description');
+      expect(movieCollHaveClass[0]).toHaveClass('card_description');
+    });
+  });
 });
